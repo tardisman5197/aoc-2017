33adfad8f383a7531dea45aca147cd73c7ba31ca
Edward <eevans1@dstl.gov.uk>
2017-12-01 13:53:33 +0000

    Removed unneeded packages

--- d1/day1.go
+++ d1/day1.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"time"
 )
 
 func check(e error) {
@@ -31,10 +30,10 @@ func main() {
 func part1(input string) {
 	total := 0
 	for i, c := range input {
-		x := int(c) - 48
-		y := int(input[(i+1)%len(input)]) - 48
+		x := int(c)
+		y := int(input[(i+1)%len(input)])
 		if x == y {
-			total += x
+			total += x - 48
 		}
 	}
 	fmt.Println(total)
@@ -43,10 +42,10 @@ func part1(input string) {
 func part2(input string) {
 	total := 0
 	for i, c := range input {
-		x := int(c) - 48
-		y := int(input[(i+(len(input)/2))%len(input)] - 48)
+		x := int(c)
+		y := int(input[(i+(len(input)/2))%len(input)])
 		if x == y {
-			total += x
+			total += x - 48
 		}
 	}
 	fmt.Println(total)
