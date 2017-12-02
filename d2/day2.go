0a59807ac1520b10f25be13975e97fb89d7041ab
Edward Evans <tardisman5197@gmail.com>
2017-12-02 16:05:31 +0000

    Day 2 part 2 done

--- d2/day2.go
+++ d2/day2.go
@@ -35,10 +35,54 @@ func main() {
 	}
 
 	part1(data)
+	part2(data)
 }
 
 func part1(data [][]int) {
+	var total int = 0
+
+	for _, row := range data {
+		min := math.MaxInt32
+		max := -min
+		for _, num := range row {
+			if num > max {
+				max = num
+			}
+			if num < min {
+				min = num
+			}
+		}
+		total += (max - min)
+	}
+	fmt.Println(total)
+}
+
+func part2(data [][]int) {
+	var total int = 0
+	for _, row := range data {
+		for i, num := range row {
+			for j := i + 1; j < len(row); j++ {
+				if num > row[j] {
+					if num%row[j] == 0 {
+						total += num / row[j]
+						break
+					}
+				} else {
+					if row[j]%num == 0 {
+						total += row[j] / num
+						break
+					}
+				}
+			}
+		}
+	}
+	fmt.Println(total)
+	return
+}
+
+func part1Conccurent(data [][]int) {
 	checksum := make(chan int, len(data))
+
 	// Calculate checksums
 	go func() {
 		for _, row := range data {
@@ -73,6 +117,44 @@ func part1(data [][]int) {
 	}
 }
 
-func part2() {
+func part2Conccurent(data [][]int) {
+	checksum := make(chan int, len(data))
 
+	// Calculate checksums
+	go func() {
+		for _, row := range data {
+			go func(row []int) {
+				for i, num := range row {
+					for j := i + 1; j < len(row); j++ {
+						if num > row[j] {
+							if num%row[j] == 0 {
+								checksum <- num / row[j]
+								return
+							}
+						} else {
+							if row[j]%num == 0 {
+								checksum <- row[j] / num
+								return
+							}
+						}
+					}
+				}
+
+			}(row)
+		}
+	}()
+
+	// Calculate total
+	var total, done int = 0, 0
+	for {
+		select {
+		case i := <-checksum:
+			done++
+			total += i
+			if done >= len(data) {
+				fmt.Println(total)
+				return
+			}
+		}
+	}
 }
