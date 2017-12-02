c249f30943e363f15ee99a6cd0f4b7268d112698
Edward Evans <tardisman5197@gmail.com>
2017-12-02 15:30:19 +0000

    Day 2 part 1 complete, with unessary conccurency

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
+
 # Advent of Code 2017
 
 My attempt at the [Advent of Code 2017](http://adventofcode.com/) in go.
 
+## Runing Files
+
+'''
+go run dayX.go <filename>
+'''
+
 ## Days
 * [Day 1](https://github.com/tardisman5197/aoc2017/blob/master/d1/day1.go)
+* [Day 2](https://github.com/tardisman5197/aoc2017/blob/master/d2/day2.go)
---
+++ d2/day2.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+  "strings"
+	"strconv"
+	"math"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	// Open File
+	f, err := os.Open(os.Args[1])
+	check(err)
+	defer f.Close()
+
+	// Parse file
+	data := make([][]int, 0)
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		row := strings.Fields(s.Text())
+		nums := make([]int, 0)
+		for _, num := range row {
+			val, _ := strconv.Atoi(num)
+			nums = append(nums, val)
+		}
+		data = append(data, nums)
+	}
+
+	part1(data)
+}
+
+func part1(data [][]int) {
+	checksum := make(chan int, len(data))
+	// Calculate checksums
+	go func() {
+		for _, row := range data {
+			go func(row []int) {
+				min := math.MaxInt32
+				max := -min
+				for _, num := range row {
+					if num > max {
+						max = num
+					}
+					if num < min {
+						min = num
+					}
+				}
+				checksum <- (max - min)
+			}(row)
+		}
+	}()
+
+	// Calculate total
+	var total, done int = 0,0
+	for {
+		select {
+			case i := <-checksum:
+				done++
+				total += i
+				if done >= len(data){
+					fmt.Println(total)
+					return
+				}
+		}
+	}
+}
+
+func part2() {
+
+}
