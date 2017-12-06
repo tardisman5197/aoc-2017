da0f04b808f3ab5a6f6ec7fca7b9b2a629ee1eec
Edward <eevans1@dstl.gov.uk>
2017-12-06 10:34:17 +0000

    Day 6 Part 2 Done

--- d6/day6.go
+++ d6/day6.go
@@ -32,11 +32,14 @@ func main() {
 		}
 	}
 
+	dataC := make([]int, len(data))
+	copy(dataC, data)
+
 	part1(data)
-	// 	part2(data)
+	part2(dataC)
 }
 
-func part1(data []int) {
+func part1(data []int) ([]int, []int) {
 	past := make([][]int, 0)
 	for {
 		// Get Lowest
@@ -58,7 +61,7 @@ func part1(data []int) {
 		for _, bank := range past {
 			if reflect.DeepEqual(data, bank) {
 				fmt.Println(len(past) + 1)
-				return
+				return data, bank
 			}
 		}
 
@@ -66,5 +69,34 @@ func part1(data []int) {
 		dataC := make([]int, len(data))
 		copy(dataC, data)
 		past = append(past, dataC)
+	}
+}
+
+func part2(data []int) {
+	var cycles = 0
+	data, pastBank := part1(data)
+	for {
+		// Get Lowest
+		var highest = 0
+		for i, bank := range data {
+			if bank > data[highest] {
+				highest = i
+			}
+		}
+
+		// Distribute blocks
+		var block = data[highest]
+		data[highest] = 0
+		for i := 1; i <= block; i++ {
+			data[(highest+i)%len(data)]++
+		}
+
+		// Check for repeat
+		if reflect.DeepEqual(data, pastBank) {
+			fmt.Println(cycles + 1)
+			return
+		}
+
+		cycles++
 	}
 }
